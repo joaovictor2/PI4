1145a372a52731715b6b6ed261d86349a12b3cd1
Carlos Henrique <carlosnunes99@hotmail.com>
2017-11-19 19:07:15 -0200

    Mudança nas funções de pesquisa de doenças

--- controller/PiController.php
+++ controller/PiController.php
@@ -5,25 +5,45 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/projinteg/model/PiModel.php";
 $piModel = new PiModel();
 
 $piModel->zeraPesquisa();
-$qtdsintomas = $piModel->analisar();
 
-for($i = 0; $i < 10; $i++){
-	$avaliado = 
-	if(isset()){
-		$piModel->selecaoSintoma(8);
-	}else{
-		echo $_GET["sintoma<?=$i?>"];
+$acao = $_GET["acao"];
+
+
+if($acao == "continuar"){
+	$raiz = $_POST["primario"];
+	$piModel->selecaoSintoma($raiz);
+	$doencasEncontradas = $piModel->usaRaiz($raiz); // seleciona todas as doencas com o sintoma raiz
+
+	foreach ($doencasEncontradas as $de) {
+		$piModel->selecionaDoenca($de["idDoenca"]); // salva todas as doencas onde a busca deverá ser efetuada
 	}
-}
 
-$doencas = $piModel->resultado();
+	$sintomasSecundarios = $piModel->listaSintomasSecundarios($raiz); // lista todos os sintomas que estão presentes nas doenças com o sintoma raiz
 
- foreach ($doencas as $d):
- 	echo "doenca = " . $d["idDoenca"];
- endforeach;
+	foreach ($sintomasSecundarios as $ss) { 
+		$cont = 0;
+		$cont = $cont + 1; ?>
+		<input class="filled-in" type="checkbox" id="sintoma<?=$cont?>" name="sintoma<?=$cont?>" value="<?= $ss["idSintoma"]?>"/>
+		<label for="sintoma<?=$cont?>"><?= $ss["DescricaoSintoma"]?></label><br>
 
+		<?php }
 
+	}else if($acao == "avaliar"){
+		$qtdsintomas = $piModel->analisar();
+		for($i = 1; $i <= $qtdsintomas; $i++){
+			if(isset($_POST["sintoma$i"])){
+				$piModel->selecaoSintoma($_POST["sintoma$i"]);
+			}
+		}
 
+	}else if($acao == "finalizar"){
+		$raiz = $_POST["primario"];
+		$piModel->selecaoSintoma($raiz);
+		$resultado = $piModel->resultado();
 
+		foreach ($resultado as $r) {
+			echo "<div class='card-panel teal lighten-2'>Você tem " . number_format ($r["Probabilidade"], 1) ."% de chance de estar com " . $r["NomeDoenca"] . "</div>";
+		}
+	}
 
-?>
+	?>
--- model/PiModel.php
+++ model/PiModel.php
@@ -6,15 +6,17 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/projinteg/config/BancoDados.php";
 class PiModel{
 
 	private $bd;
-	private $pesquisa = "";
-	private $busca = "select sintomadoenca.idDoenca, doenca.DescricaoDoenca from sintomadoenca inner join doenca on sintomadoenca.idDoenca = doenca.idDoenca where sintomadoenca.idSintoma = ";
+	private $pesquisaSintoma = "";
+	private $pesquisaDoenca = "";
+	private $busca = "select sintomadoenca.idDoenca as CodDoenca, doenca.DescricaoDoenca as NomeDoenca, ((count(*)/(select count(sintomadoenca.idSintoma) from sintomadoenca where sintomadoenca.idDoenca = CodDoenca))*100) as Probabilidade from sintomadoenca inner join doenca on sintomadoenca.idDoenca = doenca.idDoenca where sintomadoenca.idSintoma =";
+	private $fim = "group by sintomadoenca.idDoenca ORDER by Probabilidade desc";
 
 	function __construct(){
 		$this->bd = BancoDados::obterConexao();
 	}
 
 	public function listarSintomas(){
-		$consulta = $this->bd->query("select * from Sintoma order by DescricaoSintoma");
+		$consulta = $this->bd->query("select * from Sintoma where idSintoma in (5, 7, 15, 22, 23, 34, 38, 43, 55) order by DescricaoSintoma");
 
 		$sintomas = $consulta->fetchAll();
 
@@ -22,7 +24,12 @@ class PiModel{
 	}
 
 	public function analisar(){
-		$consulta = $this->bd->prepare("select count(idSintoma) from sintoma");
+		$comandoSintomas = "";
+		$comandoSintomas .= $this->pesquisaSintoma;
+		$n = strlen($comandoSintomas) - 29;
+		$comandoSintomas = substr($comandoSintomas, 0, $n);
+		$consulta = $this->bd->prepare("select count(idSintoma) from sintoma where idSintoma = :a");
+		$consulta->bindParam(":a", $comandoSintomas);
 		$consulta->execute();
 
 		$qtdsintomas = $consulta->fetch();
@@ -31,24 +38,56 @@ class PiModel{
 	}
 
 	public function selecaoSintoma($idSintoma){
-		$this->pesquisa = $this->pesquisa . " $idSintoma " . "or sintomadoenca.idSintoma = ";
+		$this->pesquisaSintoma = $this->pesquisaSintoma . " $idSintoma " . "or sintomadoenca.idSintoma = ";
+	}
+
+	public function selecionaDoenca($idDoenca){
+		$this->pesquisaDoenca = $this->pesquisaDoenca . " $idDoenca " . "or sintomadoenca.idDoenca = ";
 	}
 
 	public function zeraPesquisa(){
-		$this->pesquisa = "";
+		$this->pesquisaSintoma = "";
+		$this->pesquisaDoenca = "";
 	}
 
 	public function resultado(){
 		$comando = $this->busca;
-		$comando .= $this->pesquisa;
+		$comando .= $this->pesquisaSintoma;
 		$n = strlen($comando) - 29;
 		$comando = substr($comando, 0, $n);
+		$comando .= $this->fim;
 
 		$consulta = $this->bd->query("$comando");
 
 		$doencas = $consulta->fetchAll();
 
 		return $doencas;
+	}
+
+	public function usaRaiz($idSintoma){
+		$consultaDemaisSintomas = $this->bd->prepare("select * from sintomadoenca where idSintoma = :idSintoma");
+
+		$consultaDemaisSintomas->bindParam(":idSintoma", $idSintoma);
+
+		$consultaDemaisSintomas->execute();
+
+		$doencasEncontradas = $consultaDemaisSintomas->fetchAll();
+
+		return $doencasEncontradas;
+
+	}
+
+	public function listaSintomasSecundarios($raiz){
+		$sintomasSecundarios = "select *, s.DescricaoSintoma from sintomadoenca as sd inner join sintoma as s on s.idSintoma = sd.idSintoma where idDoenca = " . $this->pesquisaDoenca;
+		$n = strlen($sintomasSecundarios) - 28;
+		$sintomasSecundarios = substr($sintomasSecundarios, 0, $n);
+		$sintomasSecundarios .= " and sd.idSintoma <> $raiz";
+
+		$listar = $this->bd->query("$sintomasSecundarios");
+
+		$resultadoSintomas = $listar->fetchAll();
+
+		return $resultadoSintomas;
 	}
 
 }
