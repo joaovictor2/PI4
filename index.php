fd353a12d462af3f6871dad44d40846b91346f69
YgorRibeiro <ygorribeiro1997@hotmail.com>
2017-11-24 17:55:45 -0200

    Alteração att pagina

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+<?php  
+require_once $_SERVER["DOCUMENT_ROOT"] . "/projinteg/model/MedModel.php";
+
+$medModel = new MedModel();
+$consultaEspecialidade = $medModel->consultaEspecialidade();
+
+$acao = "create";
+$Nome = "";
+$CRM = "";
+$IdEspecialidade = "";
+?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
 	<meta charset="utf-8">
@@ -25,7 +37,7 @@
 	<!-- Navigation -->
 	<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
 		<div class="container">
-			<a class="navbar-brand js-scroll-trigger" href="#page-top">O que eu tenho?</a>
+			<a class="navbar-brand js-scroll-trigger" href="index.php">O que eu tenho?</a>
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu  <i class="fa fa-bars"></i></button>
 			<div class="collapse navbar-collapse" id="navbarResponsive">
 				<ul class="navbar-nav ml-auto">
@@ -53,7 +65,7 @@
 				<div class="row">
 					<div class="col-lg-8 mx-auto">
 						<h1 class="brand-heading">O que eu tenho?</h1>
-						<p class="intro-text">Frase de Efeito.<br>Created by Us.</p>
+						<p class="intro-text">Rápido. Fácil. Eficiente.<br>Created by OQT Team.</p>
 						<a href="#about" class="btn btn-circle js-scroll-trigger">
 							<i class="fa fa-angle-double-down animated"></i>
 						</a>
@@ -91,7 +103,7 @@
 
 				<div class="card-panel">
 					<form method="post" id="formsintoma">
-					<h4 class="center-align">Selecione seu sintoma mais aparente</h4>
+						<h4 class="center-align">Selecione seu sintoma mais aparente</h4>
 						<div class="container">
 							<div class="row">
 								<div class="col">
@@ -125,6 +137,9 @@
 		</form>
 
 		<div id="fim"></div>
+		<a href="#medico" class="btn btn-circle js-scroll-trigger">
+			<i class="fa fa-angle-double-down animated"></i>
+		</a>
 	</section>
 
 
@@ -135,105 +150,118 @@
 				<div class="col-lg-8 mx-auto">
 					<h2>Cadastro de Médico</h2>
 					<p>Cadastre-se para que nossos clientes saibam quem procurar após nosso pré diagnóstico.</p>
-					<form method="post" id="formmed" action="/projinteg/controller/MedController.php?">   
+
+					<form method="post" id="formmed" action="/projinteg/controller/MedController.php?acao=<?=$acao?>">   
 						<div class="row">
 							<div class="form-group col-12">
 								<label for="Nome">Nome</label>
-								<input id="nome" type="text" name="nome" class="form-control" > 
+								<input id="Nome" type="text" name="Nome" class="form-control" placeholder="Nome" required> 
 							</div>
 						</div>
 						<div class="row">  
 							<div class="form-group col-6">                                  
 								<label for="CRM">CRM</label>
-								<input id="CRM" type="number" name="CRM" class="form-control" maxlength="10">
+								<input id="CRM" type="number" name="CRM" class="form-control" placeholder="CRM" required>
 							</div>
+
 							<div class="form-group col-6">
 								<label for="especialidade">Especialidade</label>
 								<select class="form-control" name="IdEspecialidade" id="especialidade">
-									<option seleted disabled>Selecione sua especialidade</option>                    
+									<option selected disabled>Selecione sua especialidade</option>
+									<?php  foreach($consultaEspecialidade as $c):?>
+										<option value="<?=$c["IdEspecialidade"]?>"><?=$c["Descricao"]?></option>
+									<?php endforeach;?>                    
 								</select>
 							</div>
 						</div>
+
 						<br>
-						<button type="submit" class="btn btn-default" name="salvar">Salvar</button>
+						<button type="submit" class="btn btn-default">Salvar</button>
 					</form>
 					<a href="#contact" class="btn btn-circle js-scroll-trigger">
 						<i class="fa fa-angle-double-down animated"></i>
 					</a>
 				</div>
-			</div>
-		</div>
-	</section>
+			</section>
 
 
-	<!-- Contact Section -->
-	<section id="contact" class="content-section text-center">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-8 mx-auto">
-					<h2>Contato</h2>
-					<p>Conheça um pouco do nosso processo de desenvolvimento através do GitHub do nosso projeto!</p>
-					<ul class="list-inline banner-social-buttons">
-						<li class="list-inline-item">
-							<a href="https://github.com/joaovictor2/PI4" class="btn btn-default btn-lg">
-								<i class="fa fa-github fa-fw"></i>
-								<span class="network-name">Github</span>
-							</a>
-						</li>
-					</ul>
-					<h2>Desenvolvido por: </h2>
-					<ul class="list-inline banner-social-buttons">
-						<li class="list-inline-item">
-							<p>Carlos Henrique - carlosnunes@unipam.edu.br    </p>
-							<p>Fernando Amaral - fernandoamaral@unipam.edu.br </p>
-							<p>João Victor - joaovictor1@unipam.edu.br        </p>
-							<p>Stéphane Castro - stephanecs@unipam.edu.br     </p>
-							<p>Ygor Ribeiro - ygorribeiro@unipam.edu.br       </p>
-						</li>
-					</ul>
+			<!-- Contact Section -->
+			<section id="contact" class="content-section text-center">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-8 mx-auto">
+							<h2>Contato</h2>
+							<p>Conheça um pouco do nosso processo de desenvolvimento através do GitHub do nosso projeto!</p>
+							<ul class="list-inline banner-social-buttons">
+								<li class="list-inline-item">
+									<a href="https://github.com/joaovictor2/PI4" class="btn btn-default btn-lg">
+										<i class="fa fa-github fa-fw"></i>
+										<span class="network-name">Github</span>
+									</a>
+								</li>
+							</ul>
+							<h2>Desenvolvido por: </h2>
+							<ul class="list-inline banner-social-buttons">
+								<li class="list-inline-item">
+									<p>Carlos Henrique - carlosnunes@unipam.edu.br    </p>
+								</li>
+								<li class="list-inline-item">
+									<p>Fernando Amaral - fernandoamaral@unipam.edu.br </p>
+								</li>
+								<li class="list-inline-item">
+									<p>João Victor - joaovictor1@unipam.edu.br        </p>
+								</li>
+								<li class="list-inline-item">
+									<p>Stéphane Castro - stephanecs@unipam.edu.br     </p>
+								</li>
+
+								<li class="list-inline-item">
+									<p>Ygor Ribeiro - ygorribeiro@unipam.edu.br       </p>
+								</li>
+							</ul>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
-	</section>
+			</section>
 
-	<!-- Bootstrap core JavaScript -->
-	<script src="vendor/jquery/jquery.min.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<!-- Plugin JavaScript -->
-	<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-	<script type="text/javascript" src="js/jquery.maskedinput.js"></script>
-	<!-- Custom scripts for this template -->
-	<script src="js/grayscale.min.js"></script>
-	
-	<script type="text/javascript">
-		function enviar(url)
-		{
-			$.ajax({
-				url: url,
-				method: $("#formsintoma").attr("method"),
-				data: $("#formsintoma").serialize(),
-				success: function(data){
-					$("#mensagem").html(data);
-					$("#formsintoma").trigger('reset'); 
-					$("#formsintoma").hide("slow");
-				}
-			})
-		}
+			<!-- Bootstrap core JavaScript -->
+			<script src="vendor/jquery/jquery.min.js"></script>
+			<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<!-- Plugin JavaScript -->
+			<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+			<script type="text/javascript" src="js/jquery.maskedinput.js"></script>
+			<!-- Custom scripts for this template -->
+			<script src="js/grayscale.min.js"></script>
 
-		function terminar(url)
-		{
-			$.ajax({
-				url: url,
-				method: $("#formresultado").attr("method"),
-				data: $("#formresultado").serialize(),
-				success: function(data){
-					$("#fim").html(data);
-					$("#finalmente").html("<h2>Resultado do seu diagnóstico</h2>");
-					$("#formresultado").trigger('reset'); 
-					$("#formresultado").hide("slow");
+			<script type="text/javascript">
+				function enviar(url)
+				{
+					$.ajax({
+						url: url,
+						method: $("#formsintoma").attr("method"),
+						data: $("#formsintoma").serialize(),
+						success: function(data){
+							$("#mensagem").html(data);
+							$("#formsintoma").trigger('reset'); 
+							$("#formsintoma").hide("slow");
+						}
+					})
 				}
-			})
-		}	
-	</script>
-</body>
-</html>
+
+				function terminar(url)
+				{
+					$.ajax({
+						url: url,
+						method: $("#formresultado").attr("method"),
+						data: $("#formresultado").serialize(),
+						success: function(data){
+							$("#fim").html(data);
+							$("#finalmente").html("<h2>Resultado do seu diagnóstico</h2>");
+							$("#formresultado").trigger('reset'); 
+							$("#formresultado").hide("slow");
+						}
+					})
+				}	
+			</script>
+		</body>
+		</html>
